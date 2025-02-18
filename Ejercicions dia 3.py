176b61483e825948ba4fc03e3a7f68ad997caf87
Manuel <up240090@alumnos.upa.edu.mx>
2025-02-18 09:31:01 -0600

    Ultimo avance dia 3

--- Ejercicions dia 3.py
+++ Ejercicions dia 3.py
@@ -1,1 +1,28 @@
-#Ahorita lo fleto
+# Programa 3
+# Ejercicio 1
+
+edad = 20
+estatura = 1.74
+numcom = 4j
+print (type(edad), edad)
+print(type(estatura), estatura)
+print(type(numcom), numcom)
+
+#Ejercicio 2
+
+base = float(input("Introduce la base del triangulo: "))
+altura = float(input("Introduce la altura del triangulo: "))
+area = ((base * altura)/2)
+print ("El area del triangulo es: ", area)
+
+#Ejercicio 3
+
+lado_a = float(input("Introduce el lado a del triangulo: "))
+lado_b = float(input("Introduce el lado b del triangulo: "))
+lado_c = float(input("Introduce el lado c del triangulo: "))
+perimetro = (lado_a + lado_b + lado_c)
+print ("El perimetro del triangulo es de :", perimetro)
+
+#Ejercicio 4
+
+
