ae4c6341260184d6f4bc470713311cb3705a85d1
Manuel <up240090@alumnos.upa.edu.mx>
2025-03-04 09:23:53 -0600

    04/Mar/25

--- Ejercicios dia 4.py
+++ Ejercicios dia 4.py
@@ -1,5 +1,34 @@
-#Programa 4
+#Ejercicios del dia 4
 
-base = float(input("Ingresa el valor de la base: "))
-altura = float(input("Ingresa el valor de la altura: "))
-print(type(base))
+print(" ")
+# Ejercicio 1
+print("Ejercicio 1")
+print('Thirty', 'days', 'of', 'Python')
+
+print(" ")
+# Ejercicio 2
+print("Ejercicio 2")
+print('Coding', 'for', 'all')
+
+print(" ")
+# Ejercicio 3
+print("Ejercicio 3")
+company = "Coding for all"
+print("Esta cadena se usara para mas cosas ","!",company,"!")
+
+print(" ")
+# Ejercicio 4
+print("Ejercicio 4")
+print(company)
+
+print(" ")
+# Ejercicio 5
+print("Ejercicio 5")
+longitud = len(company)
+print("La longitud de la cadena es de: ", longitud)
+
+print(" ")
+# Ejercicio 6
+print("Ejercicio 6")
+cad_may = company.upper()
+print("En mayusaculas seria: ", cad_may)
