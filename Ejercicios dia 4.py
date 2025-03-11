dcd5fff0278b83ecfabcde439927e03cc35b2917
Manuel <up240090@alumnos.upa.edu.mx>
2025-03-10 19:21:00 -0600

    10/03/25

--- Ejercicios dia 4.py
+++ Ejercicios dia 4.py
@@ -38,3 +38,131 @@ print(" ")
 print("Ejercicio 7")
 cad_min = company.lower()
 print("En minusculas seria: ", cad_min)
+
+print(" ")
+# Ejercicio 8
+print("Ejercicio 8")
+cad_cap = company.capitalize()
+print("En capitales seria: ", cad_cap)
+cad_tit = company.title()
+print("En titulo seria: ", cad_tit)
+cad_swa = company.swapcase()
+print("En swapcase seria: ", cad_swa)
+#Esa funcion invierte las mayusculas y minusculas
+
+print(" ")
+# Ejercicio 9
+print("Ejercicio 9")
+primera_palabra = company.split()[0]
+print(primera_palabra)
+
+print(" ")
+# Ejercicio 10
+print("Ejercicio 10")
+#Usar la funcion "Index" para obtener la posicion  al preguntar
+print("La posision de la palabra Conding en la oracion es: ", company.index("Coding"))
+
+print(" ")
+# Ejercicio 11
+print("Ejercicio 11")
+nueva_cadena = company.replace('')
+print(" ")
+# Ejercicio 12
+print("Ejercicio 12")
+
+print(" ")
+# Ejercicio 13
+print("Ejercicio 13")
+
+print(" ")
+# Ejercicio 14
+print("Ejercicio 14")
+
+print(" ")
+# Ejercicio 15
+print("Ejercicio 15")
+
+print(" ")
+# Ejercicio 16
+print("Ejercicio 16")
+
+print(" ")
+# Ejercicio 17
+print("Ejercicio 17")
+
+print(" ")
+# Ejercicio 18
+print("Ejercicio 18")
+
+print(" ")
+# Ejercicio 19
+print("Ejercicio 19")
+
+print(" ")
+# Ejercicio 20
+print("Ejercicio 20")
+
+print(" ")
+# Ejercicio 21
+print("Ejercicio 21")
+
+print(" ")
+# Ejercicio 22
+print("Ejercicio 22")
+
+print(" ")
+# Ejercicio 23
+print("Ejercicio 23")
+
+print(" ")
+# Ejercicio 24
+print("Ejercicio 24")
+
+print(" ")
+# Ejercicio 25
+print("Ejercicio 25")
+
+print(" ")
+# Ejercicio 26
+print("Ejercicio 26")
+
+print(" ")
+# Ejercicio 27
+print("Ejercicio 27")
+
+print(" ")
+# Ejercicio 28
+print("Ejercicio 28")
+
+print(" ")
+# Ejercicio 29
+print("Ejercicio 29")
+
+print(" ")
+# Ejercicio 30
+print("Ejercicio 30")
+
+print(" ")
+# Ejercicio 31
+print("Ejercicio 31")
+
+print(" ")
+# Ejercicio 32
+print("Ejercicio 32")
+
+print(" ")
+# Ejercicio 33
+print("Ejercicio 33")
+
+print(" ")
+# Ejercicio 34
+print("Ejercicio 34")
+
+print(" ")
+# Ejercicio 35
+print("Ejercicio 35")
+
+print(" ")
+# Ejercicio 36
+print("Ejercicio 36")
+
