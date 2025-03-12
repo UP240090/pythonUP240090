fe2625aab2ea1163deb25eb626eaa7c7a0bd440a
Manuel <up240090@alumnos.upa.edu.mx>
2025-03-12 10:22:59 -0600

    12/03/25

--- Ejercicios dia 5.py
+++ Ejercicios dia 5.py
@@ -175,14 +175,47 @@ print("Ejercicio 27")
 front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
 back_end = ['Node', 'Express', 'MongoDB']
 pila_completa = front_end + back_end
-print(" ")
-indice_redux = pila_completa.index('Redux')
+indice_redux = pila_completa.index('Redux') + 1
 pila_completa.insert(indice_redux, 'Python')
-pila_completa.insert(indice)
+pila_completa.insert(indice_redux + 1,'SQL')
+print(pila_completa)
 
-# Ejercicio 28
-print("Ejercicio 28")
+print(" ")
+#Ejercicios nivel 2
+print("Ejercicios nivel 2")
 
+print(" ")
+# Ejercicio 1
+print("Ejercicio 1")
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+ages.sort()
+min_ages = ages[0]
+max_ages = ages[-1]
+print(f"Edades ordenadas: {ages}")
+print(f"Edad minima: {min_ages}, Edad maxima: {max_ages}")
+
+ages.append(min_ages)
+ages.append(max_ages)
+print(f"Lista actualizada: {ages}")
+
+ages.sort() #Ordenar despues de haber agregado 
+media = len(ages) // 2
+if len(ages) % 2 == 0:
+    mediana = (ages[media - 1]+ ages[media]) / 2
+else:
+    mediana = ages[media]
+print(f"Edad mediana: {mediana}")
+
+promedio = sum(ages) / len(ages)
+print(f"Edad promedio: {promedio}")
+
+age_rango = max_ages - min_ages
+print(f"Rango de edades: {age_rango}")
+
+min_dife = abs(min_ages - promedio)
+max_dife = abs(max_ages - promedio)
+print(f"La diferencia minima con el promedio es: {min_dife}")
+print()
 print(" ")
 # Ejercicio 29
 print("Ejercicio 29")
