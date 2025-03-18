b7ee9d7f0f80771ea211b6470a465b0fc8714877
Manuel <up240090@alumnos.upa.edu.mx>
2025-03-17 19:50:23 -0600

    Termine el dia 5

--- 05_Day/Ejercicios nivel 2.py
+++ 05_Day/Ejercicios nivel 2.py
@@ -37,7 +37,62 @@ print(f"La diferencia maxima con el promedio es: {max_dife}")
 print(" ")
 # Ejercicio 2
 print("Ejercicio 2")
-variable = "En mi casita lo termino"
-print(variable)
+paises = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Antigua and Barbuda',
+    'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain',
+    'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan',
+    'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria',
+    'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde',
+    'Central African Republic', 'Chad', 'Chile', 'China', 'Colombi', 'Comoros',
+    'Congo (Brazzaville)', 'Congo', 'Costa Rica', "Cote d'Ivoire", 'Croatia', 'Cuba',
+    'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic',
+    'East Timor (Timor Timur)', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea',
+    'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia, The',
+    'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guatemala', 'Guinea',
+    'Guinea-Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hungary', 'Iceland', 'India',
+    'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan',
+    'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Korea, North', 'Korea, South', 'Kuwait',
+    'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya',
+    'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi',
+    'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania',
+    'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Morocco',
+    'Mozambique', 'Myanmar', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand',
+    'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama',
+    'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar',
+    'Romania', 'Russia', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia',
+    'Saint Vincent', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia',
+    'Senegal', 'Serbia and Montenegro', 'Seychelles', 'Sierra Leone', 'Singapore',
+    'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'Spain',
+    'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria',
+    'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad and Tobago',
+    'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine',
+    'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan',
+    'Vanuatu', 'Vatican City', 'Venezuela', 'Vietnam', 'Yemen', 'Zambia', 'Zimbabwe']
 
+pais_medio = len(paises)
+if pais_medio % 2 == 1:
+    mitad = [paises[pais_medio // 2]]
+else:
+    pais_medio = [paises[(pais_medio // 2) - 1]]
+print("El pais del medio es: ",mitad)
 
+print(" ")
+# Ejercicio 3
+print("Ejercicio 3")
+analis_paises = len(paises)
+medio = (analis_paises + 1) // 2 #Este conjunto "(analis_paises + 1) // 2" se usa para determinar donde se va a partir el codigo, tomando en cuenta que si la lista es impar el primer grupo tenga un oais mas.
+primer_grupo = paises[:medio]
+segundo_grupo = paises[medio:]
+print("El 1er grupo es: ", primer_grupo)
+print("El 2do grupo es: ",segundo_grupo)
+
+print(" ")
+# Ejercicio 4
+print("Ejercicio 4")
+paises_2 = ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark']
+#Desempaquetar
+primero, segundo, tercero, *paises_escandinavos = paises_2
+print("Primer pais: ",primero)
+print("Segundo pais: ",segundo)
+print("Tercer pais: ",tercero)
+print("Paises escandiavos: ",paises_escandinavos)
+#"Escandinavos" se refiere al resto
