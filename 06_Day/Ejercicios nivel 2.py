2781c5f2127ec607a1d97f738ffd22b4bc161735
Manuel <up240090@alumnos.upa.edu.mx>
2025-03-17 21:18:44 -0600

    17/03/25

--- 06_Day/Ejercicios nivel 2.py
+++ 06_Day/Ejercicios nivel 2.py
@@ -7,3 +7,65 @@ print("Mi papá se llama: ", papá)
 print("Mi mamá se llama: ", mamá)
 print("Mi hermano se llama: ", hermano)
 print("Mi hermana se llama: ", hermana)
+
+print(" ")
+# Ejercicio 2
+print("Ejercicio 2")
+frutas = ("Manzana", "Platano", "Fresa")
+verduras = ("Zanahoria", "Espinaca", "Pepino")
+produ_animal = ("Leche", "Huevo", "Queso")
+alimentos_tp = frutas + verduras + produ_animal
+print(alimentos_tp)
+
+print(" ")
+# Ejercicio 3
+print("Ejercicio 3")
+alimentos_tp
+alimentos_lt = list(alimentos_tp)
+print(alimentos_lt)
+
+print(" ")
+# Ejercicio 4
+print("Ejercicio 4")
+longitud = len(alimentos_tp)
+if longitud % 2 == 0:
+    producto_central = alimentos_tp[longitud // 2 - 1: longitud // 2 + 1]
+else:
+    producto_central = alimentos_tp[longitud // 2: longitud // 2 + 1]
+
+print("El producto central es: ", producto_central)
+
+print(" ")
+# Ejercicio 5
+print("Ejercicio 5")
+alimentos_lt
+primeros_tres = alimentos_lt[:3]
+ultimos_tres = alimentos_lt[-3:]
+print("Los tres primeros alimentos son: ", primeros_tres)
+print("Los tres ultimos alimentos son: ", ultimos_tres)
+
+print(" ")
+# Ejercicio 6
+print("Ejercicio 6")
+alimentos_tp
+del alimentos_tp
+try:
+    print(alimentos_tp)
+except NameError:
+    print("La tupla 'alimentos_tp' fue eliminada")
+
+print(" ")
+# Ejercicio 7
+print("Ejercicio 7")
+paises_nordicos = ("Denmark", "Finland", "Iceland", "Norway", "Sweden")
+if "Estonia" in paises_nordicos:
+    print("Estonia es un pais nordico.")
+else:
+    print("Estonia no es un pais nordico.")
+
+if "Iceland" in paises_nordicos:
+    print("Iceland es un pais nordico.")
+else:
+    print("Iceland no es un pais nordico.")
+
+
