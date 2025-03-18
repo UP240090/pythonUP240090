8a40f6097b2ee3e345b893db303efef215d6ff56
Manuel <up240090@alumnos.upa.edu.mx>
2025-03-17 20:51:03 -0600

    17/03/25 segunda parte

--- 06_Day/Ejercicios nivel 1.py
+++ 06_Day/Ejercicios nivel 1.py
@@ -1,1 +1,40 @@
-#Nivel 1
+print(" ")
+# Ejercicio 1
+print("Ejercicio 1")
+tupla_vacia = ()
+print(tupla_vacia)
+
+print(" ")
+# Ejercicio 2
+print("Ejercicio 2")
+hermanos_hermanas = ("Alejandra", "Claudia", "Carlos", "Daniel")
+print(hermanos_hermanas)
+
+print(" ")
+# Ejercicio 3
+print("Ejercicio 3")
+hermanas = ("Alejandra", "Claudia")
+hermanos = ("Carlos", "Daniel")
+hermanos_y_hermanas = hermanas + hermanos
+print(hermanos_y_hermanas)
+
+print(" ")
+# Ejercicio 4
+print("Ejercicio 4")
+hermana = ("Reanata",)
+hermano = ("Emiliano",)
+mis_hermanos = hermana + hermano
+cant1 = len(hermano)
+cant2 = len(hermano)
+cantidad_de_hermanos = cant1 + cant2
+print("Tengo: ", cantidad_de_hermanos, "hermanos.")
+
+print(" ")
+# Ejercicio 5
+print("Ejercicio 5")
+mis_hermanos
+padres = ("Luis", "Rocio")
+familia = mis_hermanos + padres
+print("Mi familia es: ", familia)
+
+#La tupla se genera al poner una coma (,) asi se identifica que es ina tupla
--- 06_Day/Ejercicios nivel 2.py
+++ 06_Day/Ejercicios nivel 2.py
@@ -1,1 +1,9 @@
-#Nivel 2
+print(" ")
+# Ejercicio 1
+print("Ejercicio 1")
+miembros_de_mi_famila = ("Renata", "Emiliano", "Luis", "Rocio")
+hermana, hermano, papá, mamá, = miembros_de_mi_famila
+print("Mi papá se llama: ", papá)
+print("Mi mamá se llama: ", mamá)
+print("Mi hermano se llama: ", hermano)
+print("Mi hermana se llama: ", hermana)
