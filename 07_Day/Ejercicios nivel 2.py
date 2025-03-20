a02b256b0f485a6d7622f656cf2937604be24889
Manuel <up240090@alumnos.upa.edu.mx>
2025-03-19 18:55:57 -0600

    19/03/2025

--- 07_Day/Ejercicios nivel 2.py
+++ 07_Day/Ejercicios nivel 2.py
@@ -3,29 +3,44 @@ print(" ")
 print("Ejercicio 1")
 A = {19, 22, 24, 20, 25, 26}
 B = {19, 22, 20, 25, 26, 24, 28, 27}
-ab = A.union(B)
+ab = A.union(B) # "union" Une los elemntos sin duplicarlos
 print(ab)
 
 print(" ")
 # Ejercicio 2
 print("Ejercicio 2")
+interseccion_ab = A.intersection(B)
+print(interseccion_ab)
 
 print(" ")
 # Ejercicio 3
 print("Ejercicio 3")
+subconjunto = A.issubset(B)
+print(subconjunto)
 
 print(" ")
 # Ejercicio 4
 print("Ejercicio 4")
+disjuntos = A.isdisjoint(B)
+print(disjuntos)
 
 print(" ")
 # Ejercicio 5
 print("Ejercicio 5")
+A.update(B)
+print(A)
+
+B.update(A)
+print(B)
 
 print(" ")
 # Ejercicio 6
 print("Ejercicio 6")
+dif_simetrica = A.symmetric_difference(B)
+print(dif_simetrica)
 
 print(" ")
 # Ejercicio 7
 print("Ejercicio 7")
+del A
+del B
--- 07_Day/Ejercicios nivel 3.py
+++ 07_Day/Ejercicios nivel 3.py
@@ -1,11 +1,27 @@
 print(" ")
 # Ejercicio 1
 print("Ejercicio 1")
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+conjunto = set(age)
+# La lista original tiene elementos duplicados y usamos "set" para eliminar los duplicados
+print("La longitud de la lista es: ",len(age))
+print("La longitud del conjunto es: ", len(conjunto))
 
 print(" ")
 # Ejercicio 2
 print("Ejercicio 2")
+# Un "string" es una cadena de texto y se representa así
+"Hola", 'Python'
+# Una "list" es una lista y se representa así
+[1, 2, 3, 4, 5], ['Manzana', 'Naranja']
+# Una "tuple" es una tupla y se representa así
+(1, 2, 3, 4, 5), ('Rojo', 'Azul')
+# Un "set" es una colección desordenada y sin elementos duplicados y se representa así
+{1, 2, 3, 4, 5}, {'Gato', 'Perro'}
 
 print(" ")
 # Ejercicio 3
 print("Ejercicio 3")
+frase = "I am teacher and I love to inspire and teach people"
+palabras_unicas = set(frase.split())
+print("Numero de palabras unicas: ", len(palabras_unicas))
