56f485d6c78b091c5d5cba210330fd8f2e4cc1d4
Manuel <up240090@alumnos.upa.edu.mx>
2025-03-24 22:42:36 -0600

    24/032025

--- 09_Day/Ejercicios nivel 2.py
+++ 09_Day/Ejercicios nivel 2.py
@@ -1,11 +1,47 @@
 print(" ")
 # Ejercicio 1
 print("Ejercicio 1")
+cali = float(input("Ingresa tu claificacion: "))
+
+if 8 <= cali <= 10:
+    mensage = "Bieeeeen"
+elif 7 <= cali <= 8.9:
+    mensage = "Bien"
+elif 6 <= cali <= 6.9:
+    mensage = "Reprobado pa"
+elif 5 <= cali <= 5.9:
+    mensage = "Que haces con tu miserable vida?"
+elif 0 <= cali <= 4.9:
+    mensage = "Haz considerado darte de baja?" 
+else:
+    mensage = "Introduce tu calificacion"
+print(f"Considera esto amigo: {mensage}")
 
 print(" ")
 # Ejercicio 2
 print("Ejercicio 2")
+mes = input("Ingresa el mes: ").capitalize()
+
+if mes in ['Septiembre', 'Ocubre', 'Noviembre']:
+    estacion = 'Otoño'
+elif mes in ['Diciembre', 'Enero', 'Febrero']:
+    estacion = 'Invierno'
+elif mes in ['Marzo', 'Abril', 'Mayo']:
+    estacion = 'Primavera'
+elif mes in ['Junio', 'Julio', 'Agosto']:
+    estacion = 'Verano'
+else:
+    estacion = "Ingresa bien el mes"
+print(f"La estacion actual es: {estacion}")
 
 print(" ")
 # Ejercicio 3
 print("Ejercicio 3")
+frutas = ['Platano', 'Naranja', 'Mango', 'Limon']
+fruta = input("Ingresa una fruta: ").capitalize()
+
+if fruta in frutas:
+    print(f"Esta fruta ya esta en la lista {frutas}")
+else:
+    frutas.append(fruta)
+    print("La lista actualizada es esta: ", frutas)
--- 09_Day/Ejercicios nivel 3.py
+++ 09_Day/Ejercicios nivel 3.py
@@ -1,3 +1,47 @@
 print(" ")
 # Ejercicio 1
 print("Ejercicio 1")
+# Diccionario
+persona = {
+    'nombre': 'Asabeneh',
+    'apellido': 'Yetayeh',
+    'edad': 250,
+    'pais': 'Finlandia',
+    'casado': True,
+    'habilidades': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'direccion': {
+        'calle': 'Calle Espacial',
+        'codigo_postal': '02210'
+    }
+}
+
+
+#1
+if 'habilidades' in persona:
+    habilidades = persona['habilidades']
+    ind_central = len(habilidades) // 2
+    print(f"La habilidad central es: {habilidades[ind_central]}")
+
+#2
+if 'habilidades' in persona:
+    tiene_python = 'Python' in persona['habilidades']
+    print(f"La persona {'tiene' if tiene_python else 'no tiene'} la habiulidad Python")
+#Utilizamos ciclos dentro de los ciclos para definir siu la intruccion es en plural o singuar, es este caso si tiene o no tiene
+
+#3
+if 'habilidades' in persona:
+    habilidades = persona['habilidades']
+    if set(['JavaScrip', 'React']) == set(habilidades):
+        print("Es un desarrollador de front-end")
+    elif set(['Node', 'Python', 'MongoDB']).issubset(habilidades):
+        print("Es un desarrollador back-end")
+    elif set(['React', 'Node', 'MongoDB']).issubset(habilidades):
+        print("Es un desarrollador fullstack")
+    else:
+        print("No es desaarrollador conocido")
+# Usamos "issubset" para evaluar si el conjunto está contenido en las habilidades.
+
+#4
+if persona.get('casado') and persona.get('pais') == 'Finlandia':
+    print(f"{persona['nombre']}{persona['apellido']} vive en Finlandia y esta casado")
+# Utilizamos "get" para acceder a una llave ("keys") del diccionario 
