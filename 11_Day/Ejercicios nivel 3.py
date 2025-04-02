f7669fe9e6543a1fee68b98e3b7a8a95f3955f9f
Manuel <up240090@alumnos.upa.edu.mx>
2025-04-02 15:15:40 -0600

    02/04/2025

--- 11_Day/Ejercicios nivel 3.py
+++ 11_Day/Ejercicios nivel 3.py
@@ -5,15 +5,3 @@ print("Ejercicio 1")
 print(" ")
 # Ejercicio 2
 print("Ejercicio 2")
-
-print(" ")
-# Ejercicio 3
-print("Ejercicio 3")
-
-print(" ")
-# Ejercicio 4
-print("Ejercicio 4")
-
-print(" ")
-# Ejercicio 5
-print("Ejercicio 5")
