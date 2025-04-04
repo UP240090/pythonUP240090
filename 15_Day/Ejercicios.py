3cf44ba1a115b4416daf6822bda569b3b4deeebc
Manuel <up240090@alumnos.upa.edu.mx>
2025-04-03 18:29:02 -0600

    Bendito sea Anaya

--- 15_Day/Ejercicios.py
+++ 15_Day/Ejercicios.py
@@ -1,3 +1,39 @@
 print(" ")
 # Ejercicio 1
 print("Ejercicio 1")
+# 1
+print "Hola mundo"
+
+# 2
+print(edad)
+
+# 3
+edad = 25
+print(edad)
+
+# 4
+lista = [1, 2, 3]
+print(lista[5])
+
+# 5
+import maths 
+
+# 6
+import math
+print(math.PI)
+
+# 7
+usuario = {"nombre": "Manuel", "edad": 30}
+print(usuario["ciudad"])
+
+# 8
+print(4 + "3")
+
+# 9
+from math import power
+
+# 10
+print(int("12a"))
+
+# 11
+print(1 / 0)
